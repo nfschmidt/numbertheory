357a41ec8b3dec1eb22990529b85518179b9c79c
Nicolas Schmidt <nfschmidt@gmail.com>
2016-04-09 13:05:05 -0300

    Simplified gdc_linear_combination iteration process

--- divisibility.py
+++ divisibility.py
@@ -27,39 +27,15 @@ def _check_not_all_zero(numbers):
         raise ValueError('All numbers cannot be 0')
 
 def _gcd_linear_combination_non_negatives(number1, number2):
-    first_reminder = number1 % number2
-    if first_reminder == 0:
-        return 0, 1
+    x, next_x = 1, 0
+    y, next_y = 0, 1
+    reminder, next_reminder = number1, number2
 
-    quotient = number1 // number2
-    first_x = 1
-    first_y = -quotient
+    while next_reminder:
+        quotient = reminder // next_reminder
+        x, next_x = next_x, x - quotient * next_x
+        y, next_y = next_y, y - quotient * next_y
+        reminder, next_reminder = next_reminder, reminder - quotient * next_reminder
+        
+    return x, y
 
-    second_reminder = number2 % first_reminder
-
-    if second_reminder == 0:
-        return first_x, first_y
-
-    quotient = number2 // first_reminder
-    second_x = -first_x * quotient
-    second_y = 1 - first_y * quotient
-
-    return _gcd_linear_combination_iteration_result(
-        previous_reminder=first_reminder, reminder=second_reminder,
-        previous_x=first_x, current_x=second_x,
-        previous_y=first_y, current_y=second_y
-    )
-
-def _gcd_linear_combination_iteration_result(previous_reminder, reminder,
-                                             previous_x, current_x,
-                                             previous_y, current_y):
-    quotient = previous_reminder // reminder
-    reminder, previous_reminder = (previous_reminder % reminder), reminder
-    while reminder:
-        current_x, previous_x = (previous_x - current_x * quotient), current_x
-        current_y, previous_y = (previous_y - current_y * quotient), current_y
-
-        quotient = previous_reminder // reminder
-        reminder, previous_reminder = (previous_reminder % reminder), reminder
-
-    return current_x, current_y
