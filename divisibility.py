c162de0df6a0329610820b8a5a653f0ff0c7aaf3
Nicolas Schmidt <nfschmidt@gmail.com>
2016-04-09 13:40:44 -0300

    Moved _check_not_all_zeros to gdc_linear_combination

--- divisibility.py
+++ divisibility.py
@@ -8,7 +8,9 @@ def gcd(number1, number2):
 def gcd_linear_combination(number1, number2):
     '''Return g, x, y such that x*n1 + y*n2 == g and g == gcd(n1, n2).'''
 
+    _check_not_all_zero((number1, number2))
     abs_n1, abs_n2 = abs(number1), abs(number2)
+
     gcd, coeficient1, coeficient2 = _gcd_linear_combination_non_negatives(abs_n1, abs_n2)
     if number1 < 0:
         coeficient1 *= -1
@@ -22,8 +24,6 @@ def _check_not_all_zero(numbers):
         raise ValueError('All numbers cannot be 0')
 
 def _gcd_linear_combination_non_negatives(number1, number2):
-    _check_not_all_zero((number1, number2))
-
     x, next_x = 1, 0
     y, next_y = 0, 1
     reminder, next_reminder = number1, number2
